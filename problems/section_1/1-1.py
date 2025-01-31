82a36cafbd160de432a16ee8d410d9a216ee713c
celelste <sebasully@gmail.com>
2025-01-31 15:28:06 -0800

    first examples

---
+++ problems/section_1/1-1.py
@@ -0,0 +1,9 @@
+def is_even(num):
+   if num % 2 == 0:
+       return True
+   return False
+
+
+number = 1
+
+print(is_even(number))
---
+++ problems/section_1/1-1.rs
@@ -0,0 +1,13 @@
+
+
+//change the functions to create the rust code equivalent to the python code
+
+fn main() {
+    ???
+}
+
+//is_even function
+
+fn is_even(??) ?? {
+    ???
+}
